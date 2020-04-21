d560072bb59483941fa34d3f4b91bd305a437686
Raphael <raphael.sonabend.15@ucl.ac.uk>
2020-04-21 22:03:32 +0100

    initial upload

--- tests/testthat/test_dens_logspline.R
+++ tests/testthat/test_dens_logspline.R
@@ -1,7 +1,7 @@
-context("<type>.<algorithm>")
+context("dens.logspline")
 
 test_that("autotest", {
-  learner = Learner<algorithm>$new()
+  learner = LearnerDensLogspline$new()
   expect_learner(learner)
   result = run_autotest(learner)
   expect_true(result, info = result$error)
